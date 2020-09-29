182b576537cba1bd5501ed12e6a0dae572ef1f44
Nam Nguyen <nam.nguyen.node@gmail.com>
2020-09-29 17:10:08 +0700

    Oho

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -4,9 +4,7 @@ const routes: RouteConfig[] = [
   {
     path: '/',
     component: () => import('layouts/MainLayout.vue'),
-    children: [
-      { path: '', component: () => import('pages/Index.vue') }
-    ]
+    children: [{ path: '', component: () => import('pages/Index.vue') }]
   },
 
   // Always leave this as last one,
