c81b4c1f49be1bdca031e5ccdf40e355c8271600
Nam Nguyen <nam.nguyen.node@gmail.com>
2020-09-30 17:12:31 +0700

    Sample

--- src-bex/js/content-script.js
+++ src-bex/js/content-script.js
@@ -1,2 +1,11 @@
 // Content script content goes here or in activatedContentHooks (use activatedContentHooks if you need a variable
 // accessible to both the content script and inside a hook
+
+const searchInput = document.querySelector('input[aria-label="Search"');
+const searchButton = document.querySelector('button[aria-label="Search"');
+const miniviewButton = document.querySelector(
+  'button[aria-label="Miniplayer (i)"'
+);
+console.log(searchInput);
+console.log(searchButton);
+console.log(miniviewButton);
