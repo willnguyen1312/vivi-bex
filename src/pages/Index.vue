e48d6c67d2ff84bedf5b166a70e3221a37aba62b
Nam Nguyen <nam.nguyen.node@gmail.com>
2020-10-01 13:39:52 +0700

    Rolling in deep

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,44 +1,27 @@
 <template>
-  <q-page class="row items-center justify-evenly">
-    <h1>TODO nha</h1>
+  <q-page class="wrapper">
+    <q-img
+      src="https://s1.gifyu.com/images/vivi-bex.png"
+      height="100%"
+      width="100%"
+      contain
+    />
   </q-page>
 </template>
 
 <script lang="ts">
-import { Todo, Meta } from 'components/models';
 import ExampleComponent from 'components/CompositionComponent.vue';
-import { defineComponent, ref } from '@vue/composition-api';
+import { defineComponent } from '@vue/composition-api';
 
 export default defineComponent({
-  name: 'PageIndex',
-  components: { ExampleComponent },
-  setup() {
-    const todos = ref<Todo[]>([
-      {
-        id: 1,
-        content: 'ct1'
-      },
-      {
-        id: 2,
-        content: 'ct2'
-      },
-      {
-        id: 3,
-        content: 'ct3'
-      },
-      {
-        id: 4,
-        content: 'ct4'
-      },
-      {
-        id: 5,
-        content: 'ct5'
-      }
-    ]);
-    const meta = ref<Meta>({
-      totalCount: 1200
-    });
-    return { todos, meta };
-  }
+  name: 'PageIndex'
 });
 </script>
+
+<style scoped>
+.wrapper {
+  width: 100vw;
+  height: 100vh;
+  background: url('https://s1.gifyu.com/images/love95e1650272602d39.gif');
+}
+</style>
